a0231df45148c601892ea83b848edbea37c0ece9
kutukvpavel <kutukovps@my.msu.ru>
2023-06-24 07:39:15 +0300

    Fix nullabiliy (again)

--- Map.cs
+++ Map.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace ModbusRegisterMap
@@ -26,21 +27,29 @@ namespace ModbusRegisterMap
         }
         public IRegister GetHolding(string name, int index)
         {
-            return HoldingRegisters[name + index.ToString()] as IRegister;
+            if (HoldingRegisters[name + index.ToString()] is not IRegister r)
+                throw new ArgumentException("Specified holding register does not exit in this map");
+            return r;
         }
         public IRegister GetInput(string name, int index)
         {
-            return InputRegisters[name + index.ToString()] as IRegister;
+            if (InputRegisters[name + index.ToString()] is not IRegister r)
+                throw new ArgumentException("Specified input register does not exit in this map");
+            return r;
         }
         public float GetInputFloat(string name, int index = -1)
         {
             if (index >= 0) name += index.ToString();
-            return (InputRegisters[name] as Register<DevFloat>).TypedValue.Value;
+            if (InputRegisters[name] is not Register<DevFloat> res)
+                throw new ArgumentException("Specified input register does not exit or is not of type 'DevFloat'"); 
+            return res.TypedValue.Value;
         }
         public float GetHoldingFloat(string name, int index = -1)
         {
             if (index >= 0) name += index.ToString();
-            return (HoldingRegisters[name] as Register<DevFloat>).TypedValue.Value;
+            if (HoldingRegisters[name] is not Register<DevFloat> res)
+                throw new ArgumentException("Specified holding register does not exit or is not of type 'DevFloat'"); 
+            return res.TypedValue.Value;
         }
         public void AddHolding<T>(string name, int num, bool poll = false) where T : IDeviceType, new()
         {
@@ -51,12 +60,16 @@ namespace ModbusRegisterMap
             Add<T>(InputRegisters, name, num, cfg, poll, PollInputRegisters);
         }
 
-        private void Add<T>(OrderedDictionary to, string name, int num, bool cfg = false, bool poll = false, List<string> pollList = null) where T : IDeviceType, new()
+        private void Add<T>(OrderedDictionary to, string name, int num, bool cfg = false, bool poll = false, 
+            List<string>? pollList = null) where T : IDeviceType, new()
         {
             int addr = 0;
             if (to.Count > 0)
             {
-                var last = to[to.Count - 1] as IRegister; //Do not use ^1 with OrderedDictionary index-based access!! Will return NULL.
+                //Do not use ^1 with OrderedDictionary index-based access!! Will return NULL.
+#pragma warning disable IDE0056
+                if (to[to.Count - 1] is not IRegister last) throw new Exception("This cannot happen under normal circumstances!");
+#pragma warning restore IDE0056
                 addr = last.Address + last.Length;
             }
             string n = name;
@@ -66,7 +79,7 @@ namespace ModbusRegisterMap
                 var item = new Register<T>((ushort)addr, n);
                 to.Add(n, item);
                 if (cfg) ConfigRegisters.Add(n);
-                if (poll) pollList.Add(n);
+                if (poll) pollList?.Add(n);
                 addr += item.Length;
             }
         }
